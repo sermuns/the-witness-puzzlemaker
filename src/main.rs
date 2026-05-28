899a6a52b534511190804bf56fece7072c7061da
Samuel Åkesson <sermuns@lysator.liu.se>
2026-05-28 22:00:07 +0200

    add macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: env!("CARGO_PKG_NAME").into(),
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        next_frame().await
+    }
 }
